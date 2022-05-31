db2c10abfec36f87f049728c5e8a27e8168b8041
Alex Persinger <apfbacc@gmail.com>
2022-05-31 14:58:26 -0400

    Few updates from the fork

--- src/models/bot.ts
+++ src/models/bot.ts
@@ -123,6 +123,7 @@ export class Bot {
     }
 
     private async onMessage(msg: Message): Promise<void> {
+        console.log(msg)
         if (
             !this.ready ||
             (Debug.dummyMode.enabled && !Debug.dummyMode.whitelist.includes(msg.author.id))
